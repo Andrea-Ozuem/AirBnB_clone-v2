4bf9dbac801104a14d3bc33512e40aa7faf90b03
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-04-06 11:11:54 +0000

    fix: create index at correct dir

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,7 +10,7 @@ echo "<html>
   <body>
     Holberton School
   </body>
-</html>" > index.html
+</html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sed -i '38i \\tlocation \/hbnb_static\/ \{\n\t\talias \/data\/web_static\/current\/;\n\t\tautoindex off;\n\t\}' /etc/nginx/sites-available/default
