fb0bc5d09eb83d7e1f48fa3f063fdec85c3e4f20
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-04-06 18:36:59 +0000

    fix: Handle error

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,16 +11,19 @@ def do_deploy(archive_path):
     '''distributes an archive to your web servers'''
     if not os.path.exists(archive_path):
         return False
-    bname = os.path.basename(archive_path)
-    put(archive_path, '/tmp/')
-    run("mkdir -p  /data/web_static/releases/{}/".format(bname[:-4]))
-    run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(bname,
-        bname[:-4]))
-    run('rm /tmp/{}'.format(bname))
-    run('mv /data/web_static/releases/{}/web_static/* /data/web_static/\
-        releases/{}/'.format(bname[:-4], bname[:-4]))
-    run('rm -rf /data/web_static/current')
-    run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
-        .format(bname[:-4]))
-    print("New version deployed!")
-    return True
+    try:
+        bname = os.path.basename(archive_path)
+        put(archive_path, '/tmp/')
+        run("mkdir -p  /data/web_static/releases/{}/".format(bname[:-4]))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(bname,
+            bname[:-4]))
+        run('rm /tmp/{}'.format(bname))
+        run('mv /data/web_static/releases/{}/web_static/* /data/web_static/\
+            releases/{}/'.format(bname[:-4], bname[:-4]))
+        run('rm -rf /data/web_static/current')
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+            .format(bname[:-4]))
+        print("New version deployed!")
+        return True
+    except Exception:
+        return False
