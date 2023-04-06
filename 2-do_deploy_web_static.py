f80e5190a7721ab47838fbcae4d16a16a9a0a892
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-04-06 19:27:38 +0000

    Try run as sudo

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 ''' Script to br run for remote server'''
 
-from fabric.api import env, run, put
+from fabric.api import env, run, put, sudo
 import os
 
 env.hosts = ['18.209.224.36', '52.72.32.178']
 env.user = 'ubuntu'
+
 
 def do_deploy(archive_path):
     '''distributes an archive to your web servers'''
@@ -14,17 +15,17 @@ def do_deploy(archive_path):
     try:
         bname = os.path.basename(archive_path)
         put(archive_path, '/tmp/')
-        run("mkdir -p  /data/web_static/releases/{}/".format(bname[:-4]))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(bname,
-            bname[:-4]))
-        run('rm /tmp/{}'.format(bname))
-        run('mv /data/web_static/releases/{}/web_static/* /data/web_static/\
-            releases/{}/'.format(bname[:-4], bname[:-4]))
-        run('rm -rf /data/web_static/releases/{}/web_static/'.format(
+        sudo("mkdir -p  /data/web_static/releases/{}/".format(bname[:-4]))
+        sudo('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(bname,
              bname[:-4]))
-        run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
-            .format(bname[:-4]))
+        sudo('rm /tmp/{}'.format(bname))
+        sudo('mv /data/web_static/releases/{}/web_static/* /data/web_static/\
+             releases/{}/'.format(bname[:-4], bname[:-4]))
+        sudo('rm -rf /data/web_static/releases/{}/web_static/'.format(
+              bname[:-4]))
+        sudo('rm -rf /data/web_static/current')
+        sudo('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+             .format(bname[:-4]))
         print("New version deployed!")
         return True
     except Exception:
