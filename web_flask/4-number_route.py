781f18c0e9fe0773ec484bda624986c19911394c
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-04-22 20:09:11 +0000

    feature: Add specified type

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+'''a script that starts a Flask web application'''
+
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_HBNH():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_C(text):
+    text = escape(text)
+    string = text.replace('_', ' ')
+    return 'C {}'.format(string)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return '{} is a number'.format(escape(n))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
