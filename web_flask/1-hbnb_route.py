8a5d6f0d2ca01fd9ef81da43976117313672b511
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-04-22 10:46:42 +0000

    feature: Create task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,5 +8,9 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello():
 	return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_HBNH():
+    return "HBNB"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
