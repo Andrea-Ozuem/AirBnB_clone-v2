908d3bb3f3dd42e9d1102825403646f91e579261
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-04-23 22:31:59 +0100

    feature: Cities by state template

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -16,11 +16,11 @@ def teardown_db(exception):
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
+@app.route('/cities_by_states', strict_slashes=False)
+def cities():
     '''Print for each state: id & name'''
     states = list(storage.all(State).values())
-    return render_template('7-states_list.html', states=states)
+    return render_template('8-cities_by_states.html', states=states)
 
 
 if __name__ == '__main__':
