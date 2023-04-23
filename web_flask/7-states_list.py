251252a1618c26a0d10378f937f6e75eab28198d
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-04-23 21:50:08 +0100

    testing

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,7 +5,15 @@ from models import storage
 from models.state import State
 from flask import Flask, render_template
 from markupsafe import escape
+
+
 app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    '''Tear down seesion: db'''
+    storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
@@ -13,12 +21,6 @@ def states_list():
     '''Print for each state: id & name'''
     states = list(storage.all(State).values())
     return render_template('7-states_list.html', states=states)
-
-
-@app.teardown_appcontext
-def teardown_db(exception):
-    '''Tear down seesion: db'''
-    storage.close()
 
 
 if __name__ == '__main__':
